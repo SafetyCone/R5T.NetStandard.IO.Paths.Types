1bedcd1cc731aa4e95143bbf2896d023f14eb39c
MinexAutomation <minex.automation@gmail.com>
2019-08-30 11:59:45 -0400

    Added documentation.

--- source/R5T.NetStandard.IO.Paths.Types.Testing/Code/Relative Paths/Interfaces/IStringlyTypedPathOperationsProvider.cs
+++ source/R5T.NetStandard.IO.Paths.Types.Testing/Code/Relative Paths/Interfaces/IStringlyTypedPathOperationsProvider.cs
@@ -24,7 +24,9 @@ namespace R5T.NetStandard.IO.Paths.Types.Testing
         string CombineUnchecked(string pathSegment1, string pathSegment2, string directorySeparator);
 
         /// <summary>
-        /// Ensures that <see cref="directoryIndicatedPathSegment1"/> is actually directory indicated (using logic to determine path separator to use).
+        /// Combines two path segents into a single path segment, with checks to ensure that the <see cref="directoryIndicatedPathSegment1"/> is actually directory-indicated.
+        /// If the <paramref name="directoryIndicatedPathSegment1"/> is not directory-indicated, a directory-separator is determined (from among the two choices: Windows and non-Windows) and appended.
+        /// The directory-separator is determined via logic that searches for the first occurrence of one of the two directory separator
         /// </summary>
         string CombineSimple(string directoryIndicatedPathSegment1, string pathSegment2);
 
